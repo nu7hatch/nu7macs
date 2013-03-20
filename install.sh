edfeb5b169549485771a9e5abe69c9a107c42409
Chris Kowalik <chris@nu7hat.ch>
2013-03-20 10:14:07 -0300

    updated auto-complete plugin and installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #/bin/sh
 
 [ -e ~/.emacs.d ] || mkdir -p ~/.emacs.d
-cd ~/.emacs.d && git clone git://github.com/nu7hatch/nu7macs.git nu7macs
+
+cd ~/.emacs.d
+git clone git://github.com/nu7hatch/nu7macs.git nu7macs || exit 1
+
+cd nu7macs
+git submodule init
+git submodule update || exit 1
 
 echo ';; Emacs configuration for nu7s :)' > ~/.emacs
 echo '(setq nu7macs-path "~/.emacs.d/nu7macs")' >> ~/.emacs
